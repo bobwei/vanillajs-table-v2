1a21e25fc83e343e267045cbefbd87069e24665b
bobwei <bob61016@gmail.com>
2017-04-18 12:45:04 +0800

    be able to render table

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import './index.scss';
 
-const render = (el, mountNode) => {
-  mountNode.parentNode.replaceChild(el, mountNode);
+const createTable = ({ data }) => {
+  const columns = Object.keys(data[0]);
+  const tableEl = document.createElement('table');
+  const theadEl = document.createElement('tr');
+  columns
+    .forEach((column) => {
+      const columnEl = document.createElement('th');
+      columnEl.appendChild(document.createTextNode(column));
+      theadEl.appendChild(columnEl);
+    });
+  tableEl.appendChild(theadEl);
+  data
+    .forEach((row) => {
+      const rowEl = document.createElement('tr');
+      columns
+        .forEach((column) => {
+          const columnEl = document.createElement('td');
+          columnEl.appendChild(document.createTextNode(row[column]));
+          rowEl.appendChild(columnEl);
+        });
+      tableEl.appendChild(rowEl);
+    });
+  return tableEl;
 };
 
 document.addEventListener('DOMContentLoaded', () => {
-  const testEl = document.createElement('div');
-  testEl.textContent = 'Hello World';
+  const data = [...(new Array(10))].map((obj, i) => ({ id: i, title: `title ${i}` }));
+  const tableEl = createTable({ data });
   const mountNode = document.getElementById('app');
-  render(testEl, mountNode);
+  mountNode.innerHtml = '';
+  mountNode.appendChild(tableEl);
 });
